c19f747896256d528e11435725fe283577c483ea
Aryan <singlaaryan1902@gmail.com>
2023-06-26 14:30:20 +0530

    first commmit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>*Heading*</h1>
+      <p>
+        *notes*
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+console.log('Aknb')
+ReactDOM.render(<App />, document.getElementById("root"));
