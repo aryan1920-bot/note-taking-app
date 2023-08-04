dac2a19e7499529a89d2b2dc4403ff0159ddd60b
Aryan <78062772+aryan1920-bot@users.noreply.github.com>
2023-08-05 00:49:17 +0530

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Notes from "./Notes";
 
 function App() {
+  const [noteSections, setNoteSections] = useState([<Notes key={0} />]);
+
+  const addNoteSection = () => {
+    const newKey = noteSections.length;
+    setNoteSections([...noteSections, <Notes key={newKey} />]);
+  };
+
   return (
     <div>
       <Header />
-      <Notes />
-      <Notes />
-      <Notes />
-      <Notes />
-      <Notes />
-      <Notes />
-      <Notes />
-      <Notes />
+      {noteSections}
+      <button onClick={addNoteSection}>+ Add Note Section</button>
       <Footer />
     </div>
   );
